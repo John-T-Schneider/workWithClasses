482ee90c7b361cadc2c7e42ec2498aa63da5d4e2
John Schneider <john.thomas.schneider@gmail.com>
2015-10-12 16:19:11 -0400

    in class 10/12

---
+++ app/src/main/java/edu/kvcc/cis298/workwithclasses/workwithclasses/Calc.java
@@ -0,0 +1,11 @@
+package edu.kvcc.cis298.workwithclasses.workwithclasses;
+
+/**
+ * Created by jschneider5667 on 10/12/2015.
+ */
+public class Calc {
+
+    public double multiplyByFour(double numberToMultiply){
+        return (numberToMultiply*4);
+    }
+}
